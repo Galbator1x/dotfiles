06855a1cc4b8b33af4f88283cb90e808cd27a149
Ivan Filimonov <ivan.filimonov@rt.ru>
2024-06-18 20:27:43 +0400

    update

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,9 +1,6 @@
 return {
-  -- { "echasnovski/mini.indentscope", enabled = false },
-  { "folke/noice.nvim", enabled = false },
-  { "rcarriga/nvim-notify", enabled = false },
-  { "folke/flash.nvim", enabled = false },
-  -- { "ggandor/flit.nvim", enabled = false },
-  -- { "ggandor/leap.nvim", enabled = false },
-  { "bufferline.nvim", enabled = false },
+  { "folke/noice.nvim", enabled = false }, -- replaces the UI for messages, cmdline and the popupmenu
+  { "rcarriga/nvim-notify", enabled = false }, -- notification manager
+  { "folke/flash.nvim", enabled = false }, -- navigate your code with search labels, enhanced character motions, and Treesitter integration
+  { "bufferline.nvim", enabled = false }, -- buffer line (with tabpage integration)
 }
