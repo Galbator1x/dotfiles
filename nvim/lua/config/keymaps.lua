57c2708aa199313b3fc9ca3e8a1a1cb1eb10947c
Ivan Filimonov <ivan.filimonov@rt.ru>
2024-06-22 23:05:04 +0400

    update

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -10,5 +10,8 @@ vim.keymap.del('n', 'L')
 bind("n", "<leader>q", "<cmd>q<cr>", { silent = true, noremap = true, nowait = true }) -- TODO: nowait don't work
 bind("n", "n", "nzzzv")
 
-bind("n", "<leader>c", ":let @+ = expand('%')<cr>")
-bind("n", "<leader>re", ":%s///g<left><left>")
+bind("n", "<leader>c", "<cmd>let @+ = expand('%')<cr>")
+bind("n", "<leader>re", "<cmd>%s///g<left><left>")
+
+bind({'n', 'v'}, "<leader>y", '"ay', { noremap = true, silent = true })
+bind({'n', 'v'}, "<leader>p", '"ap', { noremap = true, silent = true })
