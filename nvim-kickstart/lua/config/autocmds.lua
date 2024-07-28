ab144e573a16124ad8a37dead2cca3567545da76
Ivan Filimonov <snake73w@gmail.com>
2024-07-28 19:56:02 +0400

    disable mini.statusline highlight

--- nvim-kickstart/lua/config/autocmds.lua
+++ nvim-kickstart/lua/config/autocmds.lua
@@ -17,5 +17,16 @@ vim.api.nvim_create_autocmd('ColorScheme', {
     vim.cmd 'hi VGitSignsAdd guibg=none'
     vim.cmd 'hi VgitSignsChange guibg=none'
     vim.cmd 'hi GitSignsDelete guibg=none'
+
+    vim.cmd 'hi MiniStatuslineModeNormal guibg=none'
+    vim.cmd 'hi MiniStatuslineModeInsert guibg=none'
+    vim.cmd 'hi MiniStatuslineModeVisual guibg=none'
+    vim.cmd 'hi MiniStatuslineModeReplace guibg=none'
+    vim.cmd 'hi MiniStatuslineModeCommand guibg=none'
+    vim.cmd 'hi MiniStatuslineModeOther guibg=none'
+    vim.cmd 'hi MiniStatuslineDevinfo guibg=none'
+    vim.cmd 'hi MiniStatuslineFilename guibg=none'
+    vim.cmd 'hi MiniStatuslineFileinfo guibg=none'
+    vim.cmd 'hi MiniStatuslineInactive guibg=none'
   end,
 })
--- nvim-kickstart/lua/plugins/others.lua
+++ nvim-kickstart/lua/plugins/others.lua
@@ -44,9 +44,10 @@ return {
   -- },
 
   {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    -- opts = {},
+    'MeanderingProgrammer/markdown.nvim',
+    main = 'render-markdown',
+    opts = {},
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
   },
 
   {
@@ -115,48 +116,6 @@ return {
         -- javascript = { { "prettierd", "prettier" } },
       },
     },
-  },
-
-  { -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    opts = {
-      ensure_installed = { 'ruby', 'bash', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
-    config = function(_, opts)
-      -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-
-      -- Prefer git instead of curl in order to improve connectivity in some environments
-      require('nvim-treesitter.install').prefer_git = true
-      ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup(opts)
-
-      -- There are additional nvim-treesitter modules that you can use to interact
-      -- with nvim-treesitter. You should go explore a few and see what interests you:
-      --
-      --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-      --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-      --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-    end,
-  },
-
-  {
-    'RRethy/nvim-treesitter-endwise',
-    ---@diagnostic disable: missing-fields
-    config = function()
-      require('nvim-treesitter.configs').setup {
-        endwise = { enable = true },
-      }
-    end,
   },
 
   'michaeljsmith/vim-indent-object',
