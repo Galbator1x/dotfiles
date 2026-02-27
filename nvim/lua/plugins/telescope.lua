9a4181cc966f9a8382686b3ff8734ade78e88ef9
Ivan Filimonov <snake73w@gmail.com>
2026-02-27 10:08:31 +0400

    fix search current word

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -61,15 +61,20 @@ return {
     vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = '[S]earch Files' })
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch Git Files' })
+    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch Git [F]iles' })
     vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-    vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader>,', builtin.buffers, { desc = '[ ] Find existing buffers' })
     vim.keymap.set('n', '<leader>uC', '<cmd>Telescope colorscheme enable_preview=true<cr>', { desc = 'Change colorscheme' })
+
+    vim.keymap.set('n', '<leader>sw', function()
+      require('telescope.builtin').grep_string({
+        word_match = '-w'
+      })
+    end, { desc = '[S]earch current [W]ord' })
 
     vim.keymap.set('n', '<leader>/', function()
       builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
